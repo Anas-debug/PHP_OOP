44c7a15b22daf01817db977df520091db6b80998
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-08 21:48:43 +0100

    sat the __get() magic method

--- magic_methods/getters.php
+++ magic_methods/getters.php
@@ -6,10 +6,14 @@
         public $name = "Jordan";
         private $phone = "12345678910";
 
-        public function __get($name){
-
+        public function __get($propName){
+            if($propName === 'username'){
+                return $this->name;
+            }
+            return "Property \"$propName\" does not exist";
         }
     }
 
 $person1 = new Person();
-$person1->phone;
+// $person1->phone;
+echo $person1->username;
