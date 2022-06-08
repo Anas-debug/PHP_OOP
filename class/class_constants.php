fc2c4e29bb845cadc6bed977c3122d1c5f2863aa
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-08 21:10:41 +0100

    Changed logic from normal constants to class constants.

--- class/class_constants.php
+++ class/class_constants.php
@@ -8,8 +8,24 @@ class Car{
     //class constants
     const MY_CONSTANT = [];
 
+    const MANUFACTURER_HONDA = "Honda";
+    const MANUFACTURER_LEXUS = "Lexus";
+    const MANUFACTURER_MAZDA = "Mazda";
+    const MANUFACTURER_AUDI = "Audi";
+
+    const COLOR_WHITE = "white";
+    const COLOR_Black = "black";
+    const COLOR_SILVER = "silver";
+    const COLOR_BLUE = "blue";
+
+
     static public function myTestFunction(){
         return self::MY_CONSTANT;
+    }
+
+    public function __construct($color, $manufacturer){
+        $this->color = $color;
+        $this->manifacturer = $manufacturer;
     }
 }
 
@@ -18,4 +34,8 @@ Car::MY_CONSTANT;
 
 // var_dump(Car::MY_CONSTANT);
 
-var_dump(Car::myTestFunction());
+// var_dump(Car::myTestFunction());
+
+$myCar = new Car(Car::MANUFACTURER_HONDA, Car::COLOR_SILVER);
+
+var_dump($myCar);
