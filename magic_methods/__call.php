c14ef530854700910d73a0841c484bddbe436205
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-08 22:26:00 +0100

    retracted the getPhone method in __set.php and created the __call.php file.

---
+++ magic_methods/__call.php
@@ -0,0 +1,15 @@
+<?php
+
+// It includes the __set magic method
+
+    class Person{
+        public $name = "Jordan";
+        private $phone = "12345678910";
+
+        public function __get($propName){
+            if($propName === 'username'){
+                return $this->name;
+            }
+            return "Property \"$propName\" does not exist";
+        }
+    }
--- magic_methods/__set.php
+++ magic_methods/__set.php
@@ -3,4 +3,16 @@
 class Person{
     public $name = "Jordan";
     private $phone = "12345678910";
+
+    public function __set($propName, $value){
+        // __set magic method implementation
+        // var_dump($propName, $value);
+        if($propName === 'username'){
+            $this->name = $value;
+        }
+    }
 }
+
+$person1 = new Person();
+$person1->username = "Marry";
+echo $person1->name;
